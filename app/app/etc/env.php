09f474c9d02a02be8791f02894a834e71e6a4c97
root <root@DESKTOP-C4F893D.localdomain>
2020-08-03 10:46:34 +0200

    Optymalizacje

--- app/app/etc/env.php
+++ app/app/etc/env.php
@@ -27,7 +27,7 @@ return [
         ]
     ],
     'x-frame-options' => 'SAMEORIGIN',
-    'MAGE_MODE' => 'default',
+    'MAGE_MODE' => 'production',
     'session' => [
         'save' => 'files'
     ],
